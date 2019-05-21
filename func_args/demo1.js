de0825010b9886457e0ca838b578398fc36cb45b
moonnight5 <1696442886@qq.com>
2019-05-21 21:33:03 +0800

    add

---
+++ func_args/demo1.js
@@ -0,0 +1,15 @@
+function information(name, sex, money, birthday, color, likes, follow, age) {
+  return `${name} ${sex} ${money} ${birthday} ${color} ${likes} ${follow} ${age}`
+}
+
+let user = {
+  name: '李丹',
+  sex: '女',
+  money: '1个亿',
+  birthday: '5-22',
+  color: 'yellow',
+  likes: ['看剧'],
+  follow: '周杰伦',
+  age: 16
+}
+information(user);
---
+++ func_args/demo2.js
@@ -0,0 +1,11 @@
+function information ({name,age,height}) {
+  console.log(name,age,height);
+}
+let user = {
+  name: '付豪',
+  age: 19,
+  height: 170,
+  money: '5个亿',
+  sex: '男'
+}
+information(user);
